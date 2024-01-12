ada16badf69c06c2c2cd007ce781749820e2ca94
EONRaider <agsilva@tutanota.com>
2024-01-12 14:31:56 -0300

    Implement form validation

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,32 @@
 /* eslint-disable no-undef */
-import ValidatedInputField from './validator.js';
-
-class ValidatedTextField extends ValidatedInputField {
-  evaluateField() {
-    // noinspection JSValidateTypes
-    return this.input.value.length === 0 ? this.setErrorState() : this.unsetErrorState();
-  }
-}
+import ValidatedTextField from './modules/validatedTextField.js';
+import ValidatedEmailField from './modules/validatedEmailField.js';
+import SignUpForm from './modules/signUpForm.js';
 
 const firstName = new ValidatedTextField({
   inputFieldClass: '.input-first-name',
   formClass: '.action form',
   errorMessage: 'Fist Name cannot be empty',
 });
-firstName.registerEvents();
 
 const secondName = new ValidatedTextField({
   inputFieldClass: '.input-last-name',
   formClass: '.action form',
   errorMessage: 'Last Name cannot be empty',
 });
-secondName.registerEvents();
 
 const password = new ValidatedTextField({
   inputFieldClass: '.input-password',
   formClass: '.action form',
   errorMessage: 'Password cannot be empty',
 });
-password.registerEvents();
+
+const email = new ValidatedEmailField({
+  inputFieldClass: '.input-email',
+  formClass: '.action form',
+  errorMessage: 'Looks like this is not an email',
+});
+
+new SignUpForm({ formClass: '.action form', submitButtonClass: '.action form button' })
+  .addFields([firstName, secondName, password, email])
+  .start();
